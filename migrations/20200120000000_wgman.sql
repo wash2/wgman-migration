2c3485e1cf59bbd79104092517121d8a6ef82acf
Ashley Wulber <ashtronaut@pm.me>
2021-01-22 17:47:50 -0500

    fix: fix interpretation of [u8] and refactor DB

--- migrations/20200120000000_wgman.sql
+++ migrations/20200120000000_wgman.sql
@@ -1,35 +1,44 @@
 -- Table: public.Interface
 
 -- DROP TABLE public."Interface";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE public."Interface"
+-- Table: public.interface
+
+-- DROP TABLE public.interface;
+
+CREATE TABLE public.interface
 (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
-    name text COLLATE pg_catalog."default" NOT NULL,
+    u_name text COLLATE pg_catalog."default" NOT NULL,
     port integer,
     ip inet,
     public_key character(45) COLLATE pg_catalog."default",
     fqdn character varying(253) COLLATE pg_catalog."default",
-    CONSTRAINT "Account_pkey" PRIMARY KEY (id)
+    CONSTRAINT "Account_pkey" PRIMARY KEY (id),
+    CONSTRAINT name_unique UNIQUE (u_name)
+        INCLUDE(u_name),
+    CONSTRAINT public_key_unique UNIQUE (u_name)
+        INCLUDE(u_name)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Interface"
+ALTER TABLE public.interface
     OWNER to postgres;
 
--- Table: public.InterfacePassword
+-- Table: public.interface_password
 
--- DROP TABLE public."InterfacePassword";
+-- DROP TABLE public.interface_password;
 
-CREATE TABLE public."InterfacePassword"
+CREATE TABLE public.interface_password
 (
     id uuid NOT NULL,
     password_hash character varying(128) COLLATE pg_catalog."default" NOT NULL,
     salt character varying(128) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT "InterfacePassword_pkey" PRIMARY KEY (id),
     CONSTRAINT id FOREIGN KEY (id)
-        REFERENCES public."Interface" (id) MATCH SIMPLE
+        REFERENCES public.interface (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
@@ -37,56 +46,14 @@ CREATE TABLE public."InterfacePassword"
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."InterfacePassword"
+ALTER TABLE public.interface_password
     OWNER to postgres;
 
--- Table: public.User
+-- Table: public.peer_relation
 
--- DROP TABLE public."User";
+-- DROP TABLE public.peer_relation;
 
-CREATE TABLE public."User"
-(
-    id uuid NOT NULL DEFAULT uuid_generate_v4(),
-    name text COLLATE pg_catalog."default" NOT NULL,
-    is_admin boolean NOT NULL,
-    CONSTRAINT "User_pkey" PRIMARY KEY (id)
-)
-
-Insert INTO public."User" (name, is_admin)
-Values ('admin', true);
-
-TABLESPACE pg_default;
-
-ALTER TABLE public."User"
-    OWNER to postgres;
-
--- Table: public.UserPassword
-
--- DROP TABLE public."UserPassword";
-
-CREATE TABLE public."UserPassword"
-(
-    id uuid NOT NULL,
-    password_hash character varying(128) COLLATE pg_catalog."default" NOT NULL,
-    salt character varying(128) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "Password_pkey" PRIMARY KEY (id),
-    CONSTRAINT id FOREIGN KEY (id)
-        REFERENCES public."User" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public."UserPassword"
-    OWNER to postgres;
-    
--- Table: public.PeerRelation
-
--- DROP TABLE public."PeerRelation";
-
-CREATE TABLE public."PeerRelation"
+CREATE TABLE public.peer_relation
 (
     endpoint_id uuid NOT NULL,
     peer_id uuid NOT NULL,
@@ -94,29 +61,29 @@ CREATE TABLE public."PeerRelation"
     endpoint_allowed_ip inet[] NOT NULL DEFAULT '{}'::inet[],
     peer_name text COLLATE pg_catalog."default" NOT NULL,
     endpoint_name text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT peer_relation PRIMARY KEY (endpoint_id, peer_id)
+    CONSTRAINT peer_relation_tuple PRIMARY KEY (endpoint_id, peer_id)
         INCLUDE(endpoint_id, peer_id),
     CONSTRAINT endpoint_name_unique UNIQUE (endpoint_name)
         INCLUDE(endpoint_name),
     CONSTRAINT peer_name_unique UNIQUE (peer_name)
         INCLUDE(peer_name),
     CONSTRAINT endpoint_id_foreign FOREIGN KEY (endpoint_id)
-        REFERENCES public."Interface" (id) MATCH SIMPLE
+        REFERENCES public.interface (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID,
     CONSTRAINT endpoint_name_foreign FOREIGN KEY (endpoint_name)
-        REFERENCES public."Interface" (name) MATCH SIMPLE
+        REFERENCES public.interface (u_name) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID,
     CONSTRAINT peer_id_foreign FOREIGN KEY (peer_id)
-        REFERENCES public."Interface" (id) MATCH SIMPLE
+        REFERENCES public.interface (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID,
     CONSTRAINT peer_name_foreign FOREIGN KEY (peer_name)
-        REFERENCES public."Interface" (name) MATCH SIMPLE
+        REFERENCES public.interface (u_name) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
@@ -124,5 +91,47 @@ CREATE TABLE public."PeerRelation"
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."PeerRelation"
+ALTER TABLE public.peer_relation
     OWNER to postgres;
+
+-- Table: public.admin
+
+-- DROP TABLE public.admin;
+
+CREATE TABLE public.admin
+(
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    u_name text COLLATE pg_catalog."default" NOT NULL,
+    is_root boolean NOT NULL,
+    CONSTRAINT "User_pkey" PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.admin
+    OWNER to postgres;
+
+-- Table: public.admin_password
+
+-- DROP TABLE public.admin_password;
+
+CREATE TABLE public.admin_password
+(
+    id uuid NOT NULL,
+    password_hash character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    salt character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "Password_pkey" PRIMARY KEY (id),
+    CONSTRAINT id FOREIGN KEY (id)
+        REFERENCES public.admin (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.admin_password
+    OWNER to postgres;
+
+Insert INTO public.admin (u_name, is_root)
+Values ('root', true);
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::error::Error;
 use sqlx::{Pool, Postgres, migrate::{Migrator}, postgres::PgPoolOptions};
 use std::path::Path;
-use wgman_core::{config::get_db_cfg, types::User, auth};
+use wgman_core::{config::get_db_cfg, types::Admin, auth};
 use std::env::{var};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     //db setup
+    println!("loading config");
     let db_cfg = match get_db_cfg() {
         Ok(cfg) => {cfg}
         Err(err) => {
@@ -15,21 +16,24 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     };
 
+    let db_url = &format!("postgres://{}:{}@{}:{}/{}", db_cfg.user, db_cfg.pw, db_cfg.host, db_cfg.port, db_cfg.name);
+    println!("connecting to pool at {}", db_url);
     let pool: Pool<Postgres> = PgPoolOptions::new()
     .max_connections(20)
-    .connect(&format!("postgres://{}:{}@{}:{}/{}", db_cfg.user, db_cfg.pw, db_cfg.host, db_cfg.port, db_cfg.name))
+    .connect(db_url)
     .await?;
 
+    println!("running migrations");
     let m = Migrator::new(Path::new("./migrations")).await?;
     m.run(&pool).await?;
+    let pw = var("WGMAN_DB_ROOT_PW")?;
 
-    let User { id, name: _, is_admin: _ } = sqlx::query_as::<_, User>("SELECT id FROM public.\"User\" Where name = $1")
-    .bind("admin")
+    println!("getting root");
+    let Admin { id, u_name: _, is_root: _ } = sqlx::query_as::<_, Admin>("SELECT * FROM public.admin WHERE u_name = 'root'")
     .fetch_one(&pool)
     .await?;
 
-    let pw = var("WGMAN_DB_ADMIN_PW")?;
-
+    println!("hashing pw");
     let auth::Hash { pbkdf2_hash, salt}: auth::Hash = match auth::encrypt(&pw) {
         Ok(hash) => hash,
         Err(_) => {
@@ -38,11 +42,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
         },
     };
 
-    let _rec = sqlx::query("Insert INTO public.\"UserPassword\" (id, password_hash, salt) Values ($1, $2, $3);")
+    let pbkdf2_hash: String = pbkdf2_hash.iter().map(|b| b.clone() as char).collect();
+    let salt: String = salt.iter().map(|b| b.clone() as char).collect();
+
+    println!("adding user password");
+    sqlx::query("Insert INTO public.admin_password (id, password_hash, salt) Values ($1, $2, $3);")
     .bind(id)
-    .bind(std::str::from_utf8(&pbkdf2_hash[..])?)
-    .bind(std::str::from_utf8(&salt[..])?)
-    .fetch_one(&pool)
+    .bind(pbkdf2_hash)
+    .bind(salt)
+    .execute(&pool)
     .await?;
 
     Ok(println!("Migration Complete"))
